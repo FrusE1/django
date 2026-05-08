776ea8eee61b5dcc2d8b494799110ae6793e136b
FrusE1 <fathutdinov.2000@mail.ru>
2026-05-08 13:22:29 +0300

    added functionality for detailed viewing of an article page

--- mars/news/views.py
+++ mars/news/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render, redirect
+from django.views.generic import DetailView
 
 from .forms import ArticlesForm
 from .models import Articles
 
+
 def news_home(request):
     news = Articles.objects.order_by('-date')
     return render(request, 'news/news_home.html', {'news': news})
+
+class NewsDetailView(DetailView):
+    model = Articles
+    template_name = 'news/news_detail.html'
+    context_object_name = 'article'
 
 def create(request):
     error = ''
